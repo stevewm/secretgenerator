a0899aacdffb6b52fe83fecfa93ab2c529a19130
stevewm <34654219+stevewm@users.noreply.github.com>
2024-07-20 22:48:33 -0700

    chore: tidy up

--- app/api/api.py
+++ app/api/api.py
@@ -4,4 +4,8 @@ def register_generator(generator: Type[Generator]):
     generators[generator.__name__] = generator
     print(f"Registered generator: {generator.__name__}")
 
+def deregister_generator(generator: Type[Generator]):
+    del generators[generator.__name__]
+    print(f"Deregistered generator: {generator.__name__}")
+
 generators = {}
--- app/generators/alphanumeric.py
+++ app/generators/alphanumeric.py
@@ -3,7 +3,7 @@ import string
 from typing import Any, Dict
 from ..api import api
 
-class AlphaNumeric(): # TODO: Take proper args instead of a dict and document them in the README
+class AlphaNumeric():
     def generate(parameters: Dict[str, Any]) -> str:
         return ''.join(random.choices(string.ascii_letters + string.digits, k=parameters.get('length', 32)))
 
